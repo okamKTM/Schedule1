377127abcaa2c6a965e9beea06d93aacf1c272bc
okamKTM <94751013+okamKTM@users.noreply.github.com>
2022-08-18 17:22:56 +0900

    Commit!

---
+++ ProjectSheet/MileStone_test.py
@@ -0,0 +1,13 @@
+import os
+import glob
+import openpyxl
+from openpyxl.styles.fonts import Font
+
+#Excel = glob.glob("*.xlsx")
+
+wb = openpyxl.load_workbook("Project.xlsx")
+print(wb.sheetnames)
+sheet = wb['Main']
+
+cell = sheet['A1']
+print(cell.value)
