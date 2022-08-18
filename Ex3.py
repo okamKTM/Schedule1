ae2d0121b2f8e6cd88ea16167a88b76c144eea26
okamKTM <94751013+okamKTM@users.noreply.github.com>
2022-08-18 14:36:48 +0900

    2回目のコミットです。

--- Ex3.py
+++ Ex3.py
@@ -2,4 +2,17 @@ import os
 import openpyxl
 import glob
 
+wb = openpyxl.Workbook()
+wb.create_sheet()
+Sheet = wb.sheetnames
+print(Sheet)
+
+sheet1 = wb['Sheet1']
+Sheet = wb.remove(sheet1)
+print(Sheet)
+
+
+wb.save('test.xlsx')
 print(glob.glob("*.xlsx")) #拡張子が.xlsxのものを探索
+
+
